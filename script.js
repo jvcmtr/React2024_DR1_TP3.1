51adbcb3a1c6f470e49fc65945d255977cf0729a
jvcmtr <jvictorcmtr@gmail.com>
2024-09-19 15:30:02 -0300

    done

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
+});
